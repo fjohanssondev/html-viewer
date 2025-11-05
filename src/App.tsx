46d736c92c86b0062d24c92fbf208b296662e0ad
Fredrik Johansson <fjohansson.creative@gmail.com>
2025-11-05 22:22:08 +0100

    feat: dark-mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
 import { useState } from 'react'
 import DOMPurify from 'dompurify';
+import { ThemeProvider } from '@/components/theme-provider';
 import { Textarea } from '@/components/ui/textarea'
 import { Box } from '@/components/ui/box';
+import { ModeToggle } from './components/mode-toggle';
 import './App.css'
 
 function App() {
   const [input, setInput] = useState("")
 
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center">
-      <div className="flex h-48 w-full gap-8">
-      <Textarea value={input} onChange={(e) => setInput(e.target.value)} />
-      <Box dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(input) }} />
-      </div>
-    </main>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <header>
+        <div className="flex items-center justify-between p-4">
+          <h1 className="text-3xl font-bold underline">
+          HTML/XML Viewer
+          </h1>
+          <ModeToggle />
+        </div>
+      </header>
+      <main className="min-h-screen flex flex-col items-center justify-center">
+        <div className="flex h-48 w-full gap-8">
+        <Textarea value={input} onChange={(e) => setInput(e.target.value)} />
+        <Box dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(input) }} />
+        </div>
+      </main>
+    </ThemeProvider>
   )
 }
 
--- src/components/ui/box.tsx
+++ src/components/ui/box.tsx
@@ -3,7 +3,7 @@ interface BoxProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 function Box({ children, ...props }: BoxProps) {
-  return <div {...props} className="border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm">{children}</div>;
+  return <div {...props} className="border-primary placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm">{children}</div>;
 }
 
 export { Box }
