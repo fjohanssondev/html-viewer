522f9cd3eaa8426c4689fd84fbb4cec6945ac0a7
Fredrik Johansson <fjohansson.creative@gmail.com>
2025-11-06 22:44:29 +0100

    fix: dont allow the box to keep expanding

--- src/components/ui/box.tsx
+++ src/components/ui/box.tsx
@@ -6,7 +6,7 @@ function Box({ children, ...props }: BoxProps) {
   return (
     <div
       {...props}
-      className="[&_ul]:list-disc [&_ul]:pl-6 [&_ol]:list-decimal [&_ol]:pl-6 [&_li]:list-item p-4 max-w-none dark:text-white border-primary placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 field-sizing-content min-h-64 w-full rounded-md border bg-transparent text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm prose prose-sm dark:prose-strong:text-white prose-strong:font-bold prose-custom"
+      className="[&_ul]:list-disc [&_ul]:pl-6 [&_ol]:list-decimal [&_ol]:pl-6 [&_li]:list-item p-4 max-w-none dark:text-white border-primary placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 field-sizing-content h-64 overflow-y-auto w-full rounded-md border bg-transparent text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm prose prose-sm dark:prose-strong:text-white prose-strong:font-bold prose-custom [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500"
     >
       {children}
     </div>
